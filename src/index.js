72057e2823ad0e51dc4847550fa49e77cc9f0b18
dao <gary.vo32@gmail.com>
2022-03-28 21:24:42 -0700

    added eslint and prettier to help format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.css';
 
-console.log('yo')
+console.log('yo');
 const element = document.createElement('p');
 
 element.classList.add('yoyo');
-element.textContent = 'yoyoyo'
+element.textContent = 'yoyoyo';
 
 document.body.appendChild(element);
+// const yo = 'yoyo';
