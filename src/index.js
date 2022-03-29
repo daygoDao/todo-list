62e085e39fa89cb19de4542216747ef627aa8b4d
dao <gary.vo32@gmail.com>
2022-03-28 21:39:11 -0700

    got the webpage roughly set up where things will eventually live

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './style.css';
 
+const projectList = document.querySelector('.projects');
+const project = document.createElement('li');
+project.textContent = 'project name';
+projectList.appendChild(project);
+
 console.log('yo');
 const element = document.createElement('p');
 
 element.classList.add('yoyo');
 element.textContent = 'yoyoyo';
 
-document.body.appendChild(element);
+document.querySelector('.content').appendChild(element);
 // const yo = 'yoyo';
