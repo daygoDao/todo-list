b3362f87788afb50026d55af4411bb0ed4d7713c
dao <gary.vo32@gmail.com>
2022-03-30 14:23:20 -0700

    got tasks to populate when project is clicked, need to reset before appending tasks

--- src/index.js
+++ src/index.js
@@ -27,22 +27,31 @@ listOfProjects.addProject(project1);
 listOfProjects.addProject(project2);
 // console.log(listOfProjects);
 
-//////////////////////
+///////////////////////
 // projects section //
 const projectsDOM = document.querySelector('.projects');
+
+// localStorage
+if (!localStorage.getItem('projectList')) {
+	localStorage.setItem('projectList', JSON.stringify(listOfProjects.list));
+}
+// localStorage.setItem('projectList', JSON.stringify(listOfProjects.list));
 
 // display project list
 //loop and add projects to the unordered list from the array
 for (let i = 0; i < listOfProjects.list.length; i++) {
 	const projectDOM = document.createElement('li');
 	// add event listener to catch when project section is clicked
-	const activeList = () => {
+	const activeList = (e) => {
 		// clear others
 		console.log('project clicked');
+		//console.log(e);
 		for (let i = 0; i < listOfProjects.list.length; i++) {
-			console.log(projectsDOM.children[i].classList.remove('chosenProject'));
+			projectsDOM.children[i].classList.remove('chosenProject');
 		}
 		projectDOM.classList.add('chosenProject');
+		findChosenProject();
+		displaytasks();
 	};
 
 	projectDOM.addEventListener('click', activeList);
@@ -53,6 +62,20 @@ for (let i = 0; i < listOfProjects.list.length; i++) {
 	//console.log(i);
 }
 
+function findChosenProject() {
+	// console.log(document.querySelector('.projects').children[i].className);
+	for (let i = 0; i < listOfProjects.list; i++) {
+		if (
+			document.querySelector('.projects').children[i].className ==
+			'chosenProject'
+		) {
+			console.log('found a chosen one');
+			listOfProjects.chosenProjectIndex = i;
+		}
+	}
+}
+///
+
 // add project button
 const projectInfo = () => {
 	console.log('addProjectButton clicked');
@@ -62,24 +85,34 @@ addProjectButton.textContent = '+';
 addProjectButton.addEventListener('click', projectInfo);
 projectsDOM.appendChild(addProjectButton);
 
-//////////////////
+///////////////////
 // task section //
+
 // display task list from current project selected
-for (let i = 0; i < project1.taskArray.length; i++) {
-	const taskDOM = document.createElement('li');
-	taskDOM.classList.add('yoyo');
-	taskDOM.textContent = project1.taskArray[i].title;
+//console.log(document.querySelector('.chosenProject'));
+// displaytasks();
+function displaytasks() {
+	for (
+		let i = 0;
+		i < listOfProjects.list[listOfProjects.chosenProjectIndex].taskArray.length;
+		i++
+	) {
+		//document.querySelector();
+		const taskDOM = document.createElement('li');
+		taskDOM.classList.add('task');
+		taskDOM.textContent = project1.taskArray[i].title;
 
-	document.querySelector('.content').appendChild(taskDOM);
-	// console.log(i);
+		document.querySelector('.content').appendChild(taskDOM);
+		// console.log(i);
+	}
+
+	// add tasks button
+	const taskInfo = () => {
+		console.log('add task button clicked');
+	};
+	const addTaskButton = document.createElement('button');
+	addTaskButton.addEventListener('click', taskInfo);
+	addTaskButton.textContent = '+';
+
+	document.querySelector('.content').appendChild(addTaskButton);
 }
-
-// add tasks button
-const taskInfo = () => {
-	console.log('add task button clicked');
-};
-const addTaskButton = document.createElement('button');
-addTaskButton.addEventListener('click', taskInfo);
-addTaskButton.textContent = '+';
-
-document.querySelector('.content').appendChild(addTaskButton);
--- src/project-list.js
+++ src/project-list.js
@@ -1,11 +1,12 @@
 const projectList = () => {
+	let chosenProjectIndex = 0;
 	const list = []; //to be used to store project objects
 	const addProject = (projectObj) => {
 		list.push(projectObj);
 	};
 	//function to delete obj from list
 
-	return { list, addProject };
+	return { chosenProjectIndex, list, addProject };
 };
 
 export default projectList;
